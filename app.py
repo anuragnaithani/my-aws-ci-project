035a75391877a3ef7a4924d8228d64603649ad8f
Anurag Naithani <anuragnaithani018@gmail.com>
2026-05-14 15:07:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello Anurag DevOps Project"
+    return "Hello manu DevOps Project"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
